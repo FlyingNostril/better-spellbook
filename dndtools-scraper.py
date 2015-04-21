1f813b7c29ec33bc55a44e84763c35a6cfca5c03
archaurix <archaurix@gmail.com>
2015-04-21 13:07:33 +0000

    Finished the dndtools scraper-- for now.

--- dndtools-scraper.py
+++ dndtools-scraper.py
@@ -13,5 +13,28 @@ if len(sys.argv) == 1:
 
 spell_index = sys.argv[1]
 index_file = open(spell_index)
-for line in index_file:
-    print (line.strip())
+index_html = index_file.read()
+html_handler = html2text.HTML2Text()
+html_handler.ignore_links = True
+html_handler.ignore_images = True
+html_handler.ignore_emphasis = True
+index_text = (html_handler.handle(index_html)).splitlines()
+spell_text = ""
+begin_adding = False
+#Trim out useless text first
+for line in index_text:
+    if begin_adding == True:
+        if "* ## " in line:
+            spell_text = spell_text + line[line.find("##")+3:] + "\n"
+        elif "(" in line[:1] and ")" in line[:-1]:
+           # print("Ommitting source material")
+            spell_text = line[1:-3] + spell_text + "\n"
+        elif "###" in line:
+            break
+        else:
+            spell_text = spell_text + line + "\n"
+    else:
+        if "All social disabled (faster)" in line:
+            begin_adding = True
+
+print(spell_text)
